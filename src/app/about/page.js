23bee070d076f044f4df1831e948b8dc9ba9735e
SyedaBismaa <syedabisma173@gmail.com>
2025-10-25 12:42:37 +0530

    Values updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -30,28 +30,28 @@ const About = () => {
             width={70}
             src="/projectsimg/html.png"
             alt="HTML"
-            className="absolute left-[5rem] lg:left-[14rem] top-4 lg:top-0"
+            className="absolute left-20 lg:left-56 top-4 lg:top-0"
           />
           <Image
             height={60}
             width={70}
             src="/projectsimg/css.png"
             alt="CSS"
-            className="absolute right-[5rem] lg:right-[10rem] top-4 lg:top-0"
+            className="absolute right-20 lg:right-40 top-4 lg:top-0"
           />
           <Image
             height={60}
             width={70}
             src="/projectsimg/js.png"
             alt="JavaScript"
-            className="absolute right-[5rem] lg:right-[10rem] top-36 lg:top-50"
+            className="absolute right-20 lg:right-40 top-36 lg:top-50"
           />
           <Image
             height={60}
             width={70}
             src="/projectsimg/vscode.png"
             alt="VSCode"
-            className="absolute left-[5rem] lg:left-[10rem] top-36 lg:top-50"
+            className="absolute left-20 lg:left-40 top-36 lg:top-50"
           />
         </div>
 
