a7057326b24ae3b9084c35a9f6365b5115a19adc
SyedaBismaa <syedabisma173@gmail.com>
2025-10-25 13:03:01 +0530

    Icons Upadted

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,14 +5,12 @@ const About = () => {
   return (
     <div className=" mt-5 p-6 md:p-10">
 
-      {/* Heading */}
       <div className="flex flex-col md:flex-row items-center md:items-start gap-3 md:pl-8">
         <h1 className="text-3xl md:text-5xl ">About Me</h1>
         <div className="hidden md:flex w-[70%] h-1 rounded bg-gray-700 ml-3 mt-5"></div>
         <div className="hidden md:flex h-4 w-4 bg-gray-700 rounded-full mt-4"></div>
       </div>
 
-      {/* Icons Section (always visible) */}
       <div className="flex justify-center items-center gap-5 mt-6 flex-wrap md:hidden">
         <Image height={45} width={45} src="/html.png" alt="HTML" />
         <Image height={45} width={45} src="/css.png" alt="CSS" />
@@ -20,42 +18,40 @@ const About = () => {
         <Image height={45} width={45} src="/vscode.png" alt="VSCode" />
       </div>
 
-      {/* Main Content */}
       <div className="mt-2 md:mt-20 rounded-xl p-6 relative overflow-hidden">
 
-        {/* Floating icons (desktop only, larger) */}
+
         <div className="hidden md:block relative">
           <Image
             height={60}
             width={70}
-            src="/projectsimg/html.png"
+            src="/projectsimg/htmll.png"
             alt="HTML"
             className="absolute left-20 lg:left-56 top-4 lg:top-0"
           />
           <Image
             height={60}
             width={70}
-            src="/projectsimg/css.png"
+            src="/projectsimg/csslogo.png"
             alt="CSS"
             className="absolute right-20 lg:right-40 top-4 lg:top-0"
           />
           <Image
             height={60}
             width={70}
-            src="/projectsimg/js.png"
+            src="/projectsimg/javas.png"
             alt="JavaScript"
             className="absolute right-20 lg:right-40 top-36 lg:top-50"
           />
           <Image
             height={60}
             width={70}
-            src="/projectsimg/vscode.png"
+            src="/projectsimg/vcode.png"
             alt="VSCode"
             className="absolute left-20 lg:left-40 top-36 lg:top-50"
           />
         </div>
 
-        {/* Text Content */}
         <div className="text-center md:px-16 lg:px-40 mt-5 md:mt-8">
           <h1 className="text-2xl md:text-4xl lg:text-5xl font-medium mb-8">
             [ I design & build experiences ]
